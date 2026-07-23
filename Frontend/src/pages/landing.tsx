25fad5318ae7e9bf997fa7b0c6a8cc837544105e
nyeonseok <aragorn2358@naver.com>
2026-07-23 16:29:07 +0900

    S15P11D205-60: feat: 랜딩페이지 텍스트 삽입

--- Frontend/src/pages/landing.tsx
+++ Frontend/src/pages/landing.tsx
@@ -1,10 +1,15 @@
 import StarfieldBackground from "../components/landing/StarfieldBackground";
-// import style from "../css/landing/Landing.module.css";
+import style from "../css/landing/Landing.module.css";
 
 export default function Landing() {
   return (
     <>
       <StarfieldBackground />
+      <main>
+        <span className={style.title}>Projectree</span>
+        <span className={style.info}>회의를 지식으로 변환하세요</span>
+        <button className={style.startBtn}>Get Started</button>
+      </main>
     </>
   );
 }
