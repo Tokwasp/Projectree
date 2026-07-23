1b78f31d9ee1ad4f46791898873a6cfb7e1c6b34
안현석 <aragorn2358@naver.com>
2026-07-23 17:22:56 +0900

    Revert "Merge branch 'S15P11D205-60-랜딩' into 'main'"
    This reverts merge request !5

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>,
-);
+  </StrictMode>,
+)
