9c41c0e33b670eeebcb0910c8d1e4d04645a37c1
nyeonseok <aragorn2358@naver.com>
2026-07-24 13:13:57 +0900

    S15P11D205-61: feat: 로그인 모달 UI 구현

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Landing from "./pages/Landing";
+import OAuthCallback from "./components/auth/OAuthCallback";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Landing />} />
+        <Route path="/auth/:provider/callback" element={<OAuthCallback />} />
       </Route>
     </Routes>
   );
--- Frontend/src/components/common/Header.tsx
+++ Frontend/src/components/common/Header.tsx
@@ -1,14 +1,19 @@
 import style from "../../css/components/common/Header.module.css";
 import Logo from "../../assets/logo.svg";
+import { useLoginModal } from "../../contexts/LoginModalContext";
 
 export default function Header() {
+  const { openLoginModal } = useLoginModal();
+
   return (
     <header className={style.header}>
       <div className={style.logoContainer}>
         <img src={Logo} alt="Logo" className={style.logo} />
         <span>Projectree</span>
       </div>
-      <button className={style.loginBtn}>로그인</button>
+      <button className={style.loginBtn} onClick={openLoginModal}>
+        로그인
+      </button>
     </header>
   );
 }
