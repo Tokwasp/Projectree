d92c5520a6ca4db80e0bb3efc1dd16c77ca481ef
정서영 <jszxro@gmail.com>
2026-07-25 17:21:56 +0900

    S15P11D205-74: feat: 마이페이지 UI 및 라우트 구현

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./layout/Layout";
 import AppLayout from "./layout/AppLayout";
 import Landing from "./pages/Landing";
 import HomePage from "./pages/HomePage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
       <Route element={<AppLayout />}>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Route>
     </Routes>
   );
