7e86b3d6054af447771a99e896ac294294c7b8be
조희제 <mikm7243@naver.com>
2026-07-28 16:52:49 +0900

    test: 네이버 Oauth 로그인 테스트 및 환경설정 추가

--- Backend/src/test/java/com/ssafy/projectree/IntegrationTestSupport.java
+++ Backend/src/test/java/com/ssafy/projectree/IntegrationTestSupport.java
@@ -1,6 +1,7 @@
 package com.ssafy.projectree;
 
 import com.ssafy.projectree.domain.member.service.GoogleOAuthClient;
+import com.ssafy.projectree.domain.member.service.NaverOAuthClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -11,8 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 
-    // 외부 시스템(Google OAuth) 호출은 통합 테스트에서 절대 실제로 발생시키지 않는다.
     @MockitoBean
     protected GoogleOAuthClient googleOAuthClient;
+
+    @MockitoBean
+    protected NaverOAuthClient naverOAuthClient;
 
 }
