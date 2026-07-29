2e78f5e453030e75c21d197dcc29132bdcc921d4
nyeonseok <aragorn2358@naver.com>
2026-07-30 00:33:02 +0900

    feat: 프로젝트 카테고리 엔티티 연결 및 생성 시 카테고리 저장
    - 카테고리 아이디 중복으로 들어왔을 때 Set으로 방지
    - 지정된 id 범위 밖의 값이 들어오면 ProjectErr
    예외처리

--- Backend/src/main/java/com/ssafy/projectree/domain/project/entity/Project.java
+++ Backend/src/main/java/com/ssafy/projectree/domain/project/entity/Project.java
@@ -29,6 +29,9 @@ public class Project extends BaseEntity {
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProjectMember> projectMembers = new ArrayList<>();
 
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectCategory> projectCategories = new ArrayList<>();
+
     @Builder
     private Project(String title, String content, String photoUrl) {
         this.title = title;
@@ -39,6 +42,11 @@ public class Project extends BaseEntity {
     public void addMember(ProjectMember pm) {
         this.projectMembers.add(pm);
         pm.assignProject(this);
+    }
+
+    public void addCategory(ProjectCategory pc) {
+        this.projectCategories.add(pc);
+        pc.assignProject(this);
     }
 
     public void removeMember(int memberId) {
--- Backend/src/main/java/com/ssafy/projectree/domain/project/service/ProjectService.java
+++ Backend/src/main/java/com/ssafy/projectree/domain/project/service/ProjectService.java
@@ -3,14 +3,18 @@ package com.ssafy.projectree.domain.project.service;
 import com.ssafy.projectree.domain.member.repository.MemberRepository;
 import com.ssafy.projectree.domain.project.dto.request.ProjectCreateRequest;
 import com.ssafy.projectree.domain.project.entity.Project;
+import com.ssafy.projectree.domain.project.entity.ProjectCategory;
 import com.ssafy.projectree.domain.project.entity.ProjectMember;
 import com.ssafy.projectree.domain.project.entity.ProjectRole;
 import com.ssafy.projectree.domain.project.repository.ProjectRepository;
 import com.ssafy.projectree.global.exception.CustomException;
-import com.ssafy.projectree.global.exception.CommonErrorCode;
+import com.ssafy.projectree.global.exception.ProjectErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -22,13 +26,25 @@ public class ProjectService {
     @Transactional
     public int createProject(ProjectCreateRequest request, int memberId) {
         if (!memberRepository.existsById(memberId)) {
-            throw new CustomException(CommonErrorCode.MEMBER_NOT_FOUND);
+            throw new CustomException(ProjectErrorCode.MEMBER_NOT_FOUND);
         }
 
         Project project = request.toEntity();
         ProjectMember pm = ProjectMember.createMember(memberId, ProjectRole.OWNER);
 
+        Set<Integer> categoryIds = new HashSet<>(request.getCategoryIds());
+
+        for (Integer categoryId : categoryIds) {
+            if (categoryId < 1 || categoryId > 6) {
+                throw new CustomException(ProjectErrorCode.INVALID_REQUEST);
+            }
+
+            ProjectCategory pc = ProjectCategory.createProjectCategory(categoryId);
+            project.addCategory(pc);
+        }
+
         project.addMember(pm);
+
         return projectRepository.save(project).getId();
     }
 }
