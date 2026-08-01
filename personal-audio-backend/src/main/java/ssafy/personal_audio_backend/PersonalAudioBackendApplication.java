70085c52030124cc4cc115000015aa1731e0e4f2
johuije <mikm7243@naver.com>
2026-08-01 17:55:35 +0900

    chore: spring boot 프로젝트 초기 세팅

---
+++ personal-audio-backend/src/main/java/ssafy/personal_audio_backend/PersonalAudioBackendApplication.java
@@ -0,0 +1,13 @@
+package ssafy.personal_audio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAudioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAudioBackendApplication.class, args);
+	}
+
+}
