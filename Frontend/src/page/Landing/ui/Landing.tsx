9583d39e6e55c973f5349f86705e5734f546d8b9
정서영 <jszxro@gmail.com>
2026-08-04 15:49:58 +0900

    S15P11D205-105: feat: 랜딩 페이지 소개 영역 구현

--- Frontend/src/page/Landing/ui/Landing.tsx
+++ Frontend/src/page/Landing/ui/Landing.tsx
@@ -1,3 +1,4 @@
+import AboutSection from "../components/AboutSection/AboutSection";
 import StarfieldBackground from "../components/StarfieldBackground/StarfieldBackground";
 import style from "../css/Landing.module.css";
 import { useLoginModalStore } from "../../../store/loginModalStore";
@@ -6,16 +7,27 @@ export default function Landing() {
   const { openLoginModal } = useLoginModalStore();
 
   return (
-    <>
-      <StarfieldBackground />
-      <main>
-        <span className={style.title}>Projectree</span>
-        <span className={style.info}>당신의 프로젝트를 시각화하세요</span>
-        <button className={style.startBtn} onClick={openLoginModal}>
-          Get Started
-        </button>
-      </main>
-      <div className={style.eclipseBlur} />
-    </>
+    <main className={style.landing}>
+      <section className={style.hero}>
+        <StarfieldBackground />
+
+        <div className={style.heroContent}>
+          <h1 className={style.title}>Projectree</h1>
+          <p className={style.info}>
+            <span className={style.typingText}>
+              당신의 프로젝트를 시각화하세요
+            </span>
+          </p>
+
+          <button className={style.startBtn} onClick={openLoginModal}>
+            Get Started
+          </button>
+        </div>
+
+        <div className={style.eclipseBlur} />
+      </section>
+
+      <AboutSection />
+    </main>
   );
 }
