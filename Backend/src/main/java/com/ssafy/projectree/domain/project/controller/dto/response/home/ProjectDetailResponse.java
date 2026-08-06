bcb22b61fb4beccbda7ec93a7f4d6b4dab0a5be3
조희제 <mikm7243@naver.com>
2026-08-06 16:19:18 +0900

    feat: 프로젝트 홈 조회 시 프로젝트 생성일 추가

--- Backend/src/main/java/com/ssafy/projectree/domain/project/controller/dto/response/home/ProjectDetailResponse.java
+++ Backend/src/main/java/com/ssafy/projectree/domain/project/controller/dto/response/home/ProjectDetailResponse.java
@@ -4,17 +4,22 @@ import com.ssafy.projectree.domain.project.entity.Project;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 public class ProjectDetailResponse {
     private String projectTitle;
     private String projectContent;
     private int participantCount;
+    private LocalDate projectCreatedAt;
 
     @Builder
-    private ProjectDetailResponse(String projectTitle, String projectContent, int participantCount) {
+    private ProjectDetailResponse(String projectTitle, String projectContent, int participantCount,
+                                  LocalDate projectCreatedAt) {
         this.projectTitle = projectTitle;
         this.projectContent = projectContent;
         this.participantCount = participantCount;
+        this.projectCreatedAt = projectCreatedAt;
     }
 
     public static ProjectDetailResponse of(Project project){
@@ -22,6 +27,7 @@ public class ProjectDetailResponse {
                 .projectTitle(project.getTitle())
                 .projectContent(project.getContent())
                 .participantCount(project.getProjectMembers().size())
+                .projectCreatedAt(project.getCreatedAt().toLocalDate())
                 .build();
     }
 }
